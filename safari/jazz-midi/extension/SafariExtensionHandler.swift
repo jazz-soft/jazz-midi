45661e247282f3075f68c7f24b5d1ef8130a492f
Sema <sema@pyva.net>
2020-02-19 23:47:48 -0500

    Safari - WIP

--- safari/jazz-midi/extension/SafariExtensionHandler.swift
+++ safari/jazz-midi/extension/SafariExtensionHandler.swift
@@ -4,7 +4,7 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
   override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
         page.getPropertiesWithCompletionHandler { properties in
           NSLog("Received: \(messageName) from: \(String(describing: properties?.url)) data: \(userInfo ?? [:])")
-          page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["refresh", ""]])
+          page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["refresh", ["outs" : [["name" : "Apple DLS Synth"]]]]])
         }
     }
 }

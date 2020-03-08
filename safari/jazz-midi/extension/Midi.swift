19ef9259cdb215d3f161a2923a9941f5a73e4aa0
Sema <sema@pyva.net>
2020-03-07 23:31:41 -0500

    Safari - WIP

--- safari/jazz-midi/extension/Midi.swift
+++ safari/jazz-midi/extension/Midi.swift
@@ -1,6 +1,78 @@
 import Foundation
 import AudioToolbox
 
+protocol MidiOut {
+    func send(_ : [UInt8])
+}
+
+class MidiOutDLS : MidiOut {
+  private var graph : AUGraph?
+  private var synthNode : AUNode = 0;
+  private var outNode : AUNode = 0;
+  private var synth : AudioUnit?
+  private var out : AudioUnit?
+
+  init?() {
+    NewAUGraph(&graph)
+    if (graph == nil) {
+      //print("Cannot create AUGraph");
+      return nil
+    }
+    var status : OSStatus = 0;
+    var cd = AudioComponentDescription(
+      componentType: kAudioUnitType_MusicDevice,
+      componentSubType: kAudioUnitSubType_DLSSynth,
+      componentManufacturer: kAudioUnitManufacturer_Apple,
+      componentFlags: 0,
+      componentFlagsMask: 0
+    )
+    status = AUGraphAddNode(graph!, &cd, &synthNode)
+    if (status != 0) {
+      //print("Cannot create synth node: \(status)");
+      return nil
+    }
+    cd.componentType = kAudioUnitType_Output;
+    cd.componentSubType = kAudioUnitSubType_DefaultOutput;
+    status = AUGraphAddNode(graph!, &cd, &outNode);
+    if (status != 0) {
+      //print("Cannot create output node: \(status)");
+      return nil
+    }
+    status = AUGraphOpen(graph!);
+    if (status != 0) {
+      //print("Cannot open AUGraph: \(status)");
+      return nil
+    }
+    status = AUGraphConnectNodeInput(graph!, synthNode, 0, outNode, 0);
+    if (status != 0) {
+      //print("Cannot connect nodes: \(status)");
+      return nil
+    }
+    status = AUGraphInitialize(graph!);
+    if (status != 0) {
+      //print("Cannot initialize AUGraph: \(status)");
+      return nil
+    }
+    status = AUGraphStart(graph!);
+    if (status != 0) {
+      //print("Cannot start AUGraph: \(status)");
+      return nil
+    }
+    AUGraphNodeInfo(graph!, synthNode, nil, &synth);
+    AUGraphNodeInfo(graph!, outNode, nil, &out);
+  }
+
+  deinit {
+    AUGraphStop(graph!)
+    DisposeAUGraph(graph!)
+  }
+
+  func send(_ data: [UInt8]) {
+    MusicDeviceMIDIEvent(synth!, UInt32(data[0]), UInt32(data[1]), UInt32(data[2]), 0)
+  }
+
+}
+
 class Midi {
 
   static func getDeviceInfo(_ device : MIDIEndpointRef) -> [String : String] {
--- safari/jazz-midi/extension/SafariExtensionHandler.swift
+++ safari/jazz-midi/extension/SafariExtensionHandler.swift
@@ -3,9 +3,25 @@ import SafariServices
 class SafariExtensionHandler: SFSafariExtensionHandler {
   override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
     page.getPropertiesWithCompletionHandler { properties in
-          //NSLog("Received: \(messageName) from: \(String(describing: properties?.url)) data: \(userInfo ?? [:])")
+        NSLog("Received: \(messageName) from: \(String(describing: properties?.url)) data: \(userInfo ?? [:])")
       if messageName == "refresh" {
         page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["refresh", Midi.refresh()]])
+      }
+      else if messageName == "openout" {
+        if let data = userInfo?["data"] {
+          let array = data as? [Any]
+          if let arr = array {
+            page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["openout", arr[0], arr[1]]])
+          }
+        }
+      }
+      else if messageName == "openin" {
+        if let data = userInfo?["data"] {
+          let array = data as? [Any]
+          if let arr = array {
+            page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["openin", arr[0], arr[1]]])
+          }
+        }
       }
     }
   }

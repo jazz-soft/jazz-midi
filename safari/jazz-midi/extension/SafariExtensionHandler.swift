d9b1cc5f29700e3bfb6cedbeeb6521ebf1d58cda
Sema <sema@pyva.net>
2020-03-08 01:10:22 -0500

    Safari - WIP

--- safari/jazz-midi/extension/SafariExtensionHandler.swift
+++ safari/jazz-midi/extension/SafariExtensionHandler.swift
@@ -3,24 +3,25 @@ import SafariServices
 class SafariExtensionHandler: SFSafariExtensionHandler {
   override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
     page.getPropertiesWithCompletionHandler { properties in
-        NSLog("Received: \(messageName) from: \(String(describing: properties?.url)) data: \(userInfo ?? [:])")
+        //NSLog("Received: \(messageName) from: \(String(describing: properties?.url)) data: \(userInfo ?? [:])")
       if messageName == "refresh" {
         page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["refresh", Midi.refresh()]])
       }
-      else if messageName == "openout" {
-        if let data = userInfo?["data"] {
-          let array = data as? [Any]
-          if let arr = array {
-            page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["openout", arr[0], arr[1]]])
-          }
+      else if var data = userInfo?["data"] as? [Any] {
+        let slot = data[0] as! UInt
+        data.remove(at: 0)
+        NSLog("\(messageName) \(slot) : \(data)")
+        if messageName == "send" {
         }
-      }
-      else if messageName == "openin" {
-        if let data = userInfo?["data"] {
-          let array = data as? [Any]
-          if let arr = array {
-            page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["openin", arr[0], arr[1]]])
-          }
+        else if messageName == "openout" {
+          page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["openout", slot, data[0]]])
+        }
+        else if messageName == "openin" {
+          page.dispatchMessageToScript(withName: "", userInfo: ["data" : ["openin", slot, data[0]]])
+        }
+        else if messageName == "closeout" {
+        }
+        else if messageName == "closein" {
         }
       }
     }
